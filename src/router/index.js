5c176fc9474f52a7a58aeb216568d741ada8bd7a
AdrianK98 <adrian.k.koczur@gmail.com>
2023-05-08 22:23:58 +0200

    fix
    fixed infinite redirects

--- src/router/index.js
+++ src/router/index.js
@@ -84,7 +84,7 @@ router.beforeEach((to, from, next) => {
   }
 
   if (to.matched.some(record => record.meta.requiresAuth) && !auth.currentUser) {
-    next('/')
+    next('/login')
     return;
   }
 
